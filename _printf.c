420f85fbd038186b3ac9df1ef28e35ed77714275
Sierra <SierraHunt@protonmail.com>
2026-03-16 23:49:35 +0000

    Initialised header file, main function file, and first test

---
+++ _printf.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _printf - Print a string of characters using specified format
+ *
+ * @format: Buffer containing chars and format specifiers
+ * Return: Number of characters printed
+ *
+ * Format can be a buffer of any set of characters, but
+ * "%" indicates a format specifier and following
+ * valid format characters will be replaced depending
+ * on the format
+ */
+int _printf(const char *format, ...)
+{
+	/** Variables */
+	int printed = 0;
+
+	/** Code */
+
+	/** End */
+	return (printed);
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/** Main function, _printf */
+int _printf(const char *format, ...);
+
+/** Sub-functions */
+
+
+/** Structures */
+
+#endif
