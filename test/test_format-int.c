8b8dce9ea70f014dd36548cd2c88d583ebfc1db6
Sierra <SierraHunt@protonmail.com>
2026-03-17 08:59:46 +0000

    Created some tests for basic functions

---
+++ test/test_format-int.c
@@ -0,0 +1,10 @@
+#include "../main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d", 5);
+	_printf("%d", 5);
+
+	return (0);
+}
---
+++ test/test_format-missing-conversion.c
@@ -0,0 +1,10 @@
+#include "../main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d");
+	_printf("%d");
+
+	return (0);
+}
