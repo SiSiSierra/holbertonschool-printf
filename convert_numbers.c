2373aea95b95337c63e5327d660dc1870d74e87a
Samuel Chen <sche842@gmail.com>
2026-03-18 01:09:54 +1100

    convert numbers tested and complete

--- convert_numbers.c
+++ convert_numbers.c
@@ -8,65 +8,60 @@
  */
 char *convert_char(va_list args)
 {
-	(void)args;
+(void)args;
 
-	return ("Tried to convert an char");
+return ("Tried to convert an char");
 }
 
 char *convert_int(va_list args)
 {
 /* get int from arg list */
 int n = va_arg(args, int);
-
-int i = 0;
-int tmp = n;
-int len = 0;
-char *buffer;
-int start = 0, end = 0;
-char swap;
-
+long i = 0, tmp = n, start = 0, end = 0;
+int len = 0, negative = 0;
+char *buffer, swap;
 /* handle zero case*/
 if (n == 0)
 {
 buffer = malloc(2);
-
-if (!buffer) 
-return NULL;
-
+if (!buffer)
+return (NULL);
 buffer[0] = '0';
 buffer[1] = '\0';
-
-return buffer;
+return (buffer);
 }
-
+/* convert tmp to positive and store sign */
+if (tmp < 0)
+{
+tmp = -tmp;
+negative = 1;
+}
 /* find len of number */
 while (tmp > 0)
 {
 tmp /= 10;
 len++;
 }
-
+/* reset tmp */
+tmp = n;
+if (negative == 1)
+tmp = -tmp;
 /* allocate mem for buffer */
-buffer = malloc(len + 1);
-if (!buffer) 
-return NULL;
-
+buffer = malloc(len + negative + 1);
+if (!buffer)
+return (NULL);
 /* add number in reverse */
-tmp = abs(n);
 for (i = 0; tmp > 0; i++)
 {
 buffer[i] = (tmp % 10) + '0';
 tmp /= 10;
 }
-
 /* add negative sign*/
-if (n < 0)
+if (negative == 1)
 buffer[i++] = '-';
-
 /* reverse str in place */
 start = 0;
 end = i - 1;
-
 while (start < end)
 {
 swap = buffer[start];
@@ -75,13 +70,12 @@ buffer[end] = swap;
 start++;
 end--;
 }
-
 return (buffer);
 }
 
 char *convert_str(va_list args)
 {
-	(void)args;
+(void)args;
 
-	return ("Tried to convert an str");
+return ("Tried to convert an str");
 }
--- test/test_main.c
+++ test/test_main.c
@@ -1,5 +1,6 @@
 #include "../main.h"
-#include <stdio.h>  
+#include <stdio.h> 
+#include <limits.h>
 
 /*
 compile - gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -Wno-format _printf.c convert_and_format.c convert_numbers.c test/test_main.c -o test/test_printf
@@ -9,18 +10,55 @@ run file - ./test/test_printf
 int main(void)
 {
 	int len1 = 0;
-	int len2 = 0;
-	int x = -123;
+    int len2 = 0;
+    int x;
 
-	printf("Standard printf:\n");
-	len1 = printf("test %d", x);
-	printf("\n");
-	printf("Our printf:\n");
-	len2 = _printf("test %d", x);
-	printf("\n");
+    printf("===== Integer tests =====\n");
 
-	printf("Standard printf length: %d\n", len1);
-	printf("Our printf length: %d\n", len2);	
+    /* Test negative integer */
+    x = -123;
+    printf("Standard printf:\n");
+    len1 = printf("test %d\n", x);
+    printf("Our printf:\n");
+    len2 = _printf("test %d\n", x);
+    printf("Standard printf length: %d\n", len1);
+    printf("Our printf length: %d\n\n", len2);
+
+    /* Test positive integer */
+    x = 456;
+    len1 = printf("test %d\n", x);
+    len2 = _printf("test %d\n", x);
+    printf("Standard printf length: %d\n", len1);
+    printf("Our printf length: %d\n\n", len2);
+
+    /* Test zero */
+    x = 0;
+    len1 = printf("zero: %d\n", x);
+    len2 = _printf("zero: %d\n", x);
+    printf("Standard printf length: %d\n", len1);
+    printf("Our printf length: %d\n\n", len2);
+
+    /* Test INT_MAX */
+    x = INT_MAX;
+    len1 = printf("INT_MAX: %d\n", x);
+    len2 = _printf("INT_MAX: %d\n", x);
+    printf("Standard printf length: %d\n", len1);
+    printf("Our printf length: %d\n\n", len2);
+
+    /* Test INT_MIN */
+    x = INT_MIN;
+    len1 = printf("INT_MIN: %d\n", x);
+    len2 = _printf("INT_MIN: %d\n", x);
+    printf("Standard printf length: %d\n", len1);
+    printf("Our printf length: %d\n\n", len2);
+
+    printf("===== Mixed format tests =====\n");
+
+    /* Escaped percent */
+    len1 = printf("Percent sign: %%\n");
+    len2 = _printf("Percent sign: %%\n");
+    printf("Standard printf length: %d\n", len1);
+    printf("Our printf length: %d\n\n", len2);	
 
 	return (0);
 }
