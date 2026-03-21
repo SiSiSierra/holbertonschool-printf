4f8546f4e47fc5594701fef6111d691d4b8e8d83
Samuel Chen <sche842@gmail.com>
2026-03-22 02:12:23 +1100

    test implementation

--- convert_number_uoxX.c
+++ convert_number_uoxX.c
@@ -12,22 +12,6 @@ strout_t convert_unsigned_to_base_8(va_list args, format_t format)
 {
 char specifier = 'o';
 unsigned int n = va_arg(args, unsigned int);
-(void) format;
-return (convert_num_to_base(n, specifier));
-}
-
-/**
- * convert_unsigned_to_base_10 - convert unsigned to base 10
- *
- * @args: va_list with desired int next in line
- * @format: Struct containing formatting arguments
- * Return: Pointer to converted string
- */
-
-strout_t convert_unsigned_to_base_10(va_list args, format_t format)
-{
-char specifier = 'u';
-unsigned int n = va_arg(args, unsigned int);
 strout_t base_str = convert_num_to_base(n, specifier);
 strout_t out, tmp, tmp2;
 
@@ -55,6 +39,7 @@ tmp = base_str;
 
 /* apply # flag */
 /* prepend 0 if value != 0 and first char isn't already 0 */
+/* prepend zero */
 if (format.flags.pad)
 {
 tmp2.length = tmp.length + 1;
@@ -62,21 +47,46 @@ tmp2.string = malloc(tmp2.length);
 
 tmp2.string[0] = '0';
 for (i = 0; i < tmp.length; i++)
-tmp2.string[1 + i] = tmp.string;
+tmp2.string[1 + i] = tmp.string[i];
 
-free (tmp.string);
+tmp = tmp2;
+free (tmp2.string);
 }
-/* prepend zero */
 
 /* allocate memory using get buffer, handle width */
 out = getbuffer(format.width, tmp.length);
 /* pad out depending on width */
 pad_buffer(out.string, out.length, format);
 
-/* copy tmp into out */
+/* flag left */
+/* if left copy number after padding */
+/* else copy padding starting i = 0 */
+if (format.flags.left)
+for (int i = 0; i < tmp.length; i++)
+out.string[i + format.width - tmp.length] = tmp.string[i];
+else
+for (int i = 0; i < tmp.length; i++)
+out.string[i] = tmp.string[i];
 
+free(tmp.string);
+return (out);
+}
 
+/**
+ * convert_unsigned_to_base_10 - convert unsigned to base 10
+ *
+ * @args: va_list with desired int next in line
+ * @format: Struct containing formatting arguments
+ * Return: Pointer to converted string
+ */
 
+strout_t convert_unsigned_to_base_10(va_list args, format_t format)
+{
+char specifier = 'u';
+char specifier = 'X';
+unsigned int n = va_arg(args, unsigned int);
+(void) format;
+return (convert_num_to_base(n, specifier));
 
 }
 
