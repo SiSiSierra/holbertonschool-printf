dc6084d5ca431d263375fd854285c26a69cb4baa
Samuel Chen <sche842@gmail.com>
2026-03-22 15:50:37 +1100

    flag and format complete for %u and %o

--- specifier_uoxX.c
+++ specifier_uoxX.c
@@ -12,27 +12,19 @@ strout_t convert_unsigned_to_base_8(va_list args, format_t format)
 {
 char specifier = 'o';
 unsigned int data = va_arg(args, unsigned int);
-strout_t base_str = convert_num_to_base(data, specifier);
-strout_t out, tmp;
+strout_t out, tmp, base_str;
 
-/* apply precision */
-/* check for out.length < precision */
-/* if so, apply 0 to front of string */
+/* convert string to base */
+base_str = convert_num_to_base(data, specifier);
+/* apply precision format */
 tmp = apply_precision(base_str, format);
-
 /* apply # flag */
-/* prepend 0 if value != 0 and first char isn't already 0 */
-/* prepend zero */
 tmp = apply_alternate(tmp, format);
-
 /* allocate memory using get buffer, handle width */
 out = get_buffer(format.width, tmp.length);
 /* pad out depending on width */
 pad_buffer(out.string, tmp.length, format);
-
-/* flag left */
-/* if left copy number after padding */
-/* else copy padding starting i = 0 */
+/* apply left flag */
 out = apply_left(out, tmp, format);
 
 free(tmp.string);
@@ -50,9 +42,22 @@ return (out);
 strout_t convert_unsigned_to_base_10(va_list args, format_t format)
 {
 char specifier = 'u';
-unsigned int n = va_arg(args, unsigned int);
-(void) format;
-return (convert_num_to_base(n, specifier));
+unsigned int data = va_arg(args, unsigned int);
+strout_t base_str, tmp, out;
+
+/* convert string to base */
+base_str = convert_num_to_base(data, specifier);
+/* apply precision format */
+tmp = apply_precision(base_str, format);
+/* allocate memory using get buffer, handle width */
+out = get_buffer(format.width, tmp.length);
+/* pad out depending on width */
+pad_buffer(out.string, tmp.length, format);
+/* apply left flag */
+out = apply_left(out, tmp, format);
+
+free(tmp.string);
+return (out);
 
 }
 
